71b8ade1245459befa8ad08c439dc864c5581b9a
REGINALDO MORIKAWA <reginaldo.morikawa@senacsp.edu.br>
2017-08-21 14:33:44 -0300

    first commit

--- Program.cs
+++ Program.cs
@@ -10,6 +10,19 @@ namespace IfDiferenteMediaProduto
     {
         static void Main(string[] args)
         {
+            int numero1, numero2, media, produto;
+
+            Console.Write("Digite um número: ");
+            numero1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Digite outro número: ");
+            numero2 = Convert.ToInt32(Console.ReadLine());
+            if (numero1 != numero2)
+            {
+                media = (numero1 + numero2) / 2;
+                produto = numero1 * numero2;
+                Console.Write("A média dos 2 números é " +media + " e o produto é " + produto);
+            }
+            Console.ReadLine();
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,1 @@
+"Calcula M‚dia e Produto se 2 n£meros forem diferentes C# Curso Programa‡Æo de Dispositivos M¢veis SENAC" 
